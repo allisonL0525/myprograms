903dd06025f2a8e8977ec6aa7897cce82d004127
allisonL0525 <allysnowtail567@gmail.com>
2025-09-15 10:51:53 -0400

    created a python file to be creepy

---
+++ p1.py
@@ -0,0 +1,17 @@
+#my first program 
+#created on 09/15/2025 
+#Purpose: to learn python
+#editor: Allison Lin
+
+#start quotation marks, end them
+#print and input is a function; anything that has a bracket
+#input function is weak; cant do 2 at a time and assumes everything is a sting 
+#name is a varaible 
+# ls --> list stuff
+#pwd "print working directory "
+#mkdir --> "make directory"
+#cd --> "change directory"
+
+name = input("whats your name?")
+age = input("how old are you?")
+print(f"Hello, {name} that is {age}")
